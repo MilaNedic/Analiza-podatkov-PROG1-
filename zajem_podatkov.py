f3fbc223857401cf9834a0ed2fedcb216d5afcbd
MilaNedic <roselle.freya@hotmail.com>
2019-10-27 15:09:49 +0100

    uporabila kodo z vaj

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -2,40 +2,25 @@ import requests
 import re
 import os
 import csv
+import orodja
 
 anime_na_stran = 50
 skupno_stevilo = 250
 
-#definiramo URL glavne strani z animeji 
-anime_frontpage_url = '''https://myanimelist.net/anime.php?q=&type=0&
-score=0&status=2&p=0&r=0&sm=0&sd=0&
-sy=0&em=0&ed=0&ey=0&c%5B%5D=a&c%5B%5D=b&c%5B%5D=c&c%5B%
-5D=d&c%5B%5D=e&c%5B%5D=f&c%5B%5D=g&gx=0'''
+#def filmi_na_strani(st_strani, na_stran=50):
+#    url = (
+#        f'https://myanimelist.net/anime.php?q=&type=0&score=0&status=2&p=0&r=0&sm=0&sd=0&sy=0&em=0&ed=0&ey=0&c[0]=a&c[1]=b&c[2]=c&c[3]=d&c[4]=e&c[5]=f&c[6]=g&gx=0&'
+#        f'&show={(st_strani - 1) * na_stran + 1}'
+#    )
+#    ime_datoteke = 'zajeti-podatki/najbolj-znani-filmi-{}.html'.format(st_strani)
+#    orodja.shrani_spletno_stran(url, ime_datoteke)
+#    vsebina = orodja.vsebina_datoteke(ime_datoteke)
 
-#mapa, v katero bomo shranili podatke
+# definiratje URL glavne strani bolhe za oglase z mačkami
+anime_frontpage_url = 'https://myanimelist.net/anime.php?q=&type=0&score=0&status=2&p=0&r=0&sm=0&sd=0&sy=0&em=0&ed=0&ey=0&c[0]=a&c[1]=b&c[2]=c&c[3]=d&c[4]=e&c[5]=f&c[6]=g&gx=0&show=0'
+# mapa, v katero bomo shranili podatke
 anime_directory = 'anime'
-
-#ime datoteke, v katero bomo shranili strani s podatki
-page_filename = 'anime.html'
-
-#ime CSV datoteke, v katero bomo shranili podatke
+# ime datoteke v katero bomo shranili glavno stran
+frontpage_filename = 'index_anime.html'
+# ime CSV datoteke v katero bomo shranili podatke
 csv_filename = 'anime.csv'
-
-def download_url_to_string(url):
-    '''Funkcije kot argument sprejme niz,
-    skuša vrniti vsebino te speltne strani kot niz.
-    Če pride do napake, naj vrne None'''
-    try:
-        #del kode, ki mora sprožiti napako
-        r = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        # koda, ki se izvede pri napaki
-        # dovolj je če izpišemo opozorilo in prekinemo izvajanje funkcije
-        print("Napaka pri povezovanju do:", url)
-        return None
-    # nadaljujemo s kodo če ni prišlo do napake
-    if r.status_code == requests.codes.ok:
-        return r.text
-    else:
-        print("Napaka pri prenosu strani:", url)
-        return None
